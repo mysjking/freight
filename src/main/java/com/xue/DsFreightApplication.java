7b0a6633f81d582c70726a079249007f25a1d386
mysjking <244647909@qq.com>
2021-03-22 22:45:25 +0800

    添加启动日志

--- src/main/java/com/xue/DsFreightApplication.java
+++ src/main/java/com/xue/DsFreightApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DsFreightApplication {
 
 	public static void main(String[] args) {
+		System.out.println("==启动==springboot");
 		SpringApplication.run(DsFreightApplication.class, args);
 	}
 
